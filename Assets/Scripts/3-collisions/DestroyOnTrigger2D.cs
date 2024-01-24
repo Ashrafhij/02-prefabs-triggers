ee930798bfbf1189acb3b2e88ffb1a1d50520c4a
Ashraf Hijazi <74490789+Ashrafhij@users.noreply.github.com>
2024-01-24 04:47:19 +0200

    lab3

--- Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
+++ Assets/Scripts/3-collisions/DestroyOnTrigger2D.cs
@@ -1,22 +1,56 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-/**
- * This component destroys its object whenever it triggers a 2D collider with the given tag.
- */
-public class DestroyOnTrigger2D : MonoBehaviour {
-    [Tooltip("Every object tagged with this tag will trigger the destruction of this object")]
+public class DestroyOnTrigger2D : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger the reduction of health")]
     [SerializeField] string triggeringTag;
 
-    private void OnTriggerEnter2D(Collider2D other) {
-        if (other.tag == triggeringTag && enabled) {
-            Destroy(this.gameObject);
-            Destroy(other.gameObject);
+    [Tooltip("Maximum number of shots allowed before the object is destroyed")]
+    [SerializeField] int maxShots = 3;
+
+    [Tooltip("Reference to the UI Text component for displaying health")]
+    [SerializeField] TextMesh healthText;
+
+    [Tooltip("Reference to the UI Text component for displaying health")]
+    public Text healthText2; // This line declares healthText as a public variable of type Text
+
+
+    private int currentShots = 0;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
+            // Increase the shot count
+            currentShots++;
+
+            // Update health text
+            UpdateHealthText();
+
+            // Check if the maximum number of shots has been reached
+            if (currentShots >= maxShots)
+            {
+                Destroy(this.gameObject); // Destroy the object
+            }
+
+            Destroy(other.gameObject); // Destroy the colliding object (e.g., bullet)
         }
     }
 
-    private void Update() {
+    private void UpdateHealthText()
+    {
+        if (healthText != null)
+        {
+            // Update the health text
+            healthText.text = "Health: " + (maxShots - currentShots);
+        }
+    }
+
+    private void Update()
+    {
         /* Just to show the enabled checkbox in Editor */
     }
 }
